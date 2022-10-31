634cae282839dd454c7dc9d1fdb3692ebe3c99db
Rod Kristian J. Rebarbas <103944695+rodkristianrebarbas@users.noreply.github.com>
2022-10-31 12:50:23 +0800

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -12,6 +12,86 @@ int l4 = 5;
 int l5 = 6;
 int l6 = 7;
 
+//Round Robin Left to Right Function
+void roundrobinltr(){
+  //Round Robin Left to Right
+  //1s Delay
+  
+  //Looping Sequence
+  for(int i=0;i<6;i++){
+    if(i==1){
+      digitalWrite(l1, HIGH);//on
+      delay(1000);
+      digitalWrite(l1, LOW);//off
+    }
+    if(i==2){
+      digitalWrite(l2, HIGH);//on
+      delay(1000);
+      digitalWrite(l2, LOW);//off
+    }
+    if(i==3){
+      digitalWrite(l3, HIGH);//on
+      delay(1000);
+      digitalWrite(l3, LOW);//off
+    }
+    if(i==4){
+      digitalWrite(l4, HIGH);//on
+      delay(1000);
+      digitalWrite(l4, LOW);//off
+    }
+    if(i==5){
+      digitalWrite(l5, HIGH);//on
+      delay(1000);
+      digitalWrite(l5, LOW);//off
+    }
+    if(i==6){
+      digitalWrite(l6, HIGH);//on
+      delay(1000);
+      digitalWrite(l6, LOW);//off
+    }
+  }
+}
+
+//Round Robin Right to Left Function
+void roundrobinrtl(){
+  //Round Robin Right to Left
+  //1s Delay
+  
+  //Looping Sequence
+  for(int i=6;i>0;i--){
+    if(i==6){
+      digitalWrite(l6, HIGH);//on
+      delay(1000);
+      digitalWrite(l6, LOW);//off
+    }
+    if(i==5){
+      digitalWrite(l5, HIGH);//on
+      delay(1000);
+      digitalWrite(l5, LOW);//off
+    }
+    if(i==4){
+      digitalWrite(l4, HIGH);//on
+      delay(1000);
+      digitalWrite(l4, LOW);//off
+    }
+    if(i==3){
+      digitalWrite(l3, HIGH);//on
+      delay(1000);
+      digitalWrite(l3, LOW);//off
+    }
+    if(i==2){
+      digitalWrite(l2, HIGH);//on
+      delay(1000);
+      digitalWrite(l2, LOW);//off
+    }
+    if(i==1){
+      digitalWrite(l1, HIGH);//on
+      delay(1000);
+      digitalWrite(l1, LOW);//off
+    }
+  }
+}
+
 //Setup
 void setup()
 {
@@ -27,44 +107,8 @@ void setup()
 //Loop executes repeatedly
 void loop()
 {
-  //Round Robin Left to Right
-  //1s Delay
-  digitalWrite(l1, HIGH);//on
-  delay(1000);
-  digitalWrite(l1, LOW);//off
-  digitalWrite(l2, HIGH);//on
-  delay(1000);
-  digitalWrite(l2, LOW);//off
-  digitalWrite(l3, HIGH);//on
-  delay(1000);
-  digitalWrite(l3, LOW);//off
-  digitalWrite(l4, HIGH);//on
-  delay(1000);
-  digitalWrite(l4, LOW);//off
-  digitalWrite(l5, HIGH);//on
-  delay(1000);
-  digitalWrite(l5, LOW);//off
-  digitalWrite(l6, HIGH);//on
-  delay(1000);
-  digitalWrite(l6, LOW);//off
-  //Round Robin Right to Left
-  //1s Delay
-  digitalWrite(l6, HIGH);//on
-  delay(1000);
-  digitalWrite(l6, LOW);//off
-  digitalWrite(l5, HIGH);//on
-  delay(1000);
-  digitalWrite(l5, LOW);//off
-  digitalWrite(l4, HIGH);//on
-  delay(1000);
-  digitalWrite(l4, LOW);//off
-  digitalWrite(l3, HIGH);//on
-  delay(1000);
-  digitalWrite(l3, LOW);//off
-  digitalWrite(l2, HIGH);//on
-  delay(1000);
-  digitalWrite(l2, LOW);//off
-  digitalWrite(l1, HIGH);//on
-  delay(1000);
-  digitalWrite(l1, LOW);//off
+  //Executes roundrobinltr() function
+  roundrobinltr();
+  //Executes roundrobinrtl() function
+  roundrobinrtl();
 }

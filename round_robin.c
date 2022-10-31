b48598d5980acad45505742eff034aa576b57b21
Rod Kristian J. Rebarbas <103944695+rodkristianrebarbas@users.noreply.github.com>
2022-10-31 11:46:08 +0800

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -4,6 +4,7 @@
 //Activity #6
 //Round Robin Code
 
+//Declaring variables for the LEDs globally
 int l1 = 2;
 int l2 = 3;
 int l3 = 4;
@@ -11,6 +12,7 @@ int l4 = 5;
 int l5 = 6;
 int l6 = 7;
 
+//Setup
 void setup()
 {
   pinMode(l1, OUTPUT);
@@ -25,42 +27,42 @@ void loop()
 {
   //Round Robin Left to Right
   //1s Delay
-  digitalWrite(l1, HIGH);
+  digitalWrite(l1, HIGH);//on
   delay(1000);
-  digitalWrite(l1, LOW);
-  digitalWrite(l2, HIGH);
+  digitalWrite(l1, LOW);//off
+  digitalWrite(l2, HIGH);//on
   delay(1000);
-  digitalWrite(l2, LOW);
-  digitalWrite(l3, HIGH);
+  digitalWrite(l2, LOW);//off
+  digitalWrite(l3, HIGH);//on
   delay(1000);
-  digitalWrite(l3, LOW);
-  digitalWrite(l4, HIGH);
+  digitalWrite(l3, LOW);//off
+  digitalWrite(l4, HIGH);//on
   delay(1000);
-  digitalWrite(l4, LOW);
-  digitalWrite(l5, HIGH);
+  digitalWrite(l4, LOW);//off
+  digitalWrite(l5, HIGH);//on
   delay(1000);
-  digitalWrite(l5, LOW);
-  digitalWrite(l6, HIGH);
+  digitalWrite(l5, LOW);//off
+  digitalWrite(l6, HIGH);//on
   delay(1000);
-  digitalWrite(l6, LOW);
+  digitalWrite(l6, LOW);//off
   //Round Robin Right to Left
   //1s Delay
-  digitalWrite(l6, HIGH);
+  digitalWrite(l6, HIGH);//on
   delay(1000);
-  digitalWrite(l6, LOW);
-  digitalWrite(l5, HIGH);
+  digitalWrite(l6, LOW);//off
+  digitalWrite(l5, HIGH);//on
   delay(1000);
-  digitalWrite(l5, LOW);
-  digitalWrite(l4, HIGH);
+  digitalWrite(l5, LOW);//off
+  digitalWrite(l4, HIGH);//on
   delay(1000);
-  digitalWrite(l4, LOW);
-  digitalWrite(l3, HIGH);
+  digitalWrite(l4, LOW);//off
+  digitalWrite(l3, HIGH);//on
   delay(1000);
-  digitalWrite(l3, LOW);
-  digitalWrite(l2, HIGH);
+  digitalWrite(l3, LOW);//off
+  digitalWrite(l2, HIGH);//on
   delay(1000);
-  digitalWrite(l2, LOW);
-  digitalWrite(l1, HIGH);
+  digitalWrite(l2, LOW);//off
+  digitalWrite(l1, HIGH);//on
   delay(1000);
-  digitalWrite(l1, LOW);
+  digitalWrite(l1, LOW);//off
 }
